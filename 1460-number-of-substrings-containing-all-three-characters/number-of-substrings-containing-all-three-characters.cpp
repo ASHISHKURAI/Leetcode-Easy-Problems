3ea1a4171b6c83c085a85b7a2abd140b344dac2b
Ashish Kumar Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-06-30 18:30:06 +0530

    Time: 43 ms (8.23%) | Memory: 16.6 MB (15.58%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int i=0,j=0,ans=0;
+        int n=s.size();
+        unordered_map<char,int>mp;
+        while(j<s.size()){
+            mp[s[j]]++;
+            while(mp.size()==3){
+                ans+=(n-j);
+                mp[s[i]]--;
+                if(mp[s[i]]==0)mp.erase(s[i]);
+                i++;
+            }
+            j++;
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
