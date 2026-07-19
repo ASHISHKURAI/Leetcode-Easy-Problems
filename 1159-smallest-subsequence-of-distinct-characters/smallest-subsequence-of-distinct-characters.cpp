7d303db2df7fd7d747fabf88210bb12cc25458e1
Ashish K Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2026-07-19 15:08:09 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (17.14%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        
+        vector<int> charCnt(26, 0);
+        unordered_map<char, int> inStack;
+
+        int uniqueCharCnt = 0;
+
+        for(auto ch: s) {
+
+            if(charCnt[ch - 'a'] == 0) {
+                uniqueCharCnt++;
+            }
+
+            charCnt[ch - 'a']++;
+        }
+
+        stack<char> st;
+
+        for(auto ch: s) {
+
+            charCnt[ch - 'a']--;
+            if(inStack.find(ch) != inStack.end()) {
+                continue;
+            }
+
+            while(!st.empty() && charCnt[st.top() - 'a'] != 0 && (ch < st.top())) {
+                inStack.erase(st.top());
+                st.pop();
+            }
+
+            st.push(ch);
+            inStack[ch]++;
+        }
+
+        string str = "";
+        while(!st.empty()) {
+            str += st.top();
+            st.pop();
+        }
+
+        reverse(str.begin(), str.end());
+        return str;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
